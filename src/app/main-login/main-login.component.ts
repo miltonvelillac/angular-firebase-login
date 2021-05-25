827257a581cb91f3769431360dba14336c545123
milton.velilla <milton.velillac@gmail.com>
2021-05-25 18:39:48 -0500

    box animations have been creted

--- src/app/main-login/main-login.component.ts
+++ src/app/main-login/main-login.component.ts
@@ -1,13 +1,67 @@
-import { Component, OnInit } from '@angular/core';
+import { animate, keyframes, state, style, transition, trigger } from '@angular/animations';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+
+export const sesionStates  = {
+  signIn: {
+    state: 'signIn',    
+    image: `assets/img/pexels-michael-block-3225517.jpg`
+  },
+  signUp: {
+    state: 'signUp',
+    image: `assets/img/pexels-jacob-colvin-1761279.jpg`
+  }
+  
+};
+
 
 @Component({
   selector: 'app-main-login',
   templateUrl: './main-login.component.html',
-  styleUrls: ['./main-login.component.scss']
+  styleUrls: ['./main-login.component.scss'],
+  animations: [
+    trigger('textContent', [
+      state(sesionStates.signIn.state, style({
+        transform: 'translateX(0)',
+        backgroundImage: `url(${sesionStates.signIn.image})`,
+        backgroundSize: 'cover'
+      })),
+      state(sesionStates.signUp.state, style({
+        transform: 'translateX(143%)',
+        backgroundImage: `url(${sesionStates.signUp.image})`,
+        backgroundSize: 'cover'
+      })),
+      transition(`${sesionStates.signIn.state} => ${sesionStates.signUp.state}`, [
+        animate('2s', keyframes([
+          style({ opacity: 0.5, transform: 'translateX(70%)', backgroundImage: `url(${sesionStates.signUp.image})`, offset: 0.5}),
+          style({ opacity: 0.9, transform: 'translateX(143%)', offset: 1})
+        ]))
+      ]),
+      transition(`${sesionStates.signUp.state} => ${sesionStates.signIn.state}`, [
+        animate('2s', keyframes([
+          style({ opacity: 0.5, transform: 'translateX(70%)', width: '400px', backgroundImage: `url(${sesionStates.signIn.image})`, offset: 0.5 }),
+          style({ opacity: 0.9, transform: 'translateX(0)', offset: 1 })
+        ]))
+      ])      
+    ]),
+    trigger('formContent', [
+      state(sesionStates.signIn.state, style({
+        transform: 'translateX(0)'
+      })),
+      state(sesionStates.signUp.state, style({
+        transform: 'translateX(-70%)'
+      })),
+      transition(`* => ${sesionStates.signIn.state}`, [
+        animate('2s')
+      ]),
+      transition(`* => ${sesionStates.signUp.state}`, [
+        animate('2s')
+      ])   
+    ])
+  ]
 })
 export class MainLoginComponent implements OnInit {
 
-  
+  isSignIn = true;
 
   constructor() { }
 
--- src/app/main-login/main-login.module.ts
+++ src/app/main-login/main-login.module.ts
@@ -1,17 +1,25 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+
+import {MatButtonModule} from '@angular/material/button';
+
 import { MainLoginRoutingModule } from './main-login-routing.module';
 import { MainLoginComponent } from './main-login.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 
 
 @NgModule({
   declarations: [
-    MainLoginComponent
+    MainLoginComponent,
+    SignInComponent,
+    SignUpComponent
   ],
   imports: [
     CommonModule,
-    MainLoginRoutingModule
+    MainLoginRoutingModule,
+    MatButtonModule
   ]
 })
 export class MainLoginModule { }
