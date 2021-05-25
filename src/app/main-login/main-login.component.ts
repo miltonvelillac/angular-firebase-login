00e8920c5ea0c12f8cab2a10878e8932ac8eff6b
milton.velilla <milton.velillac@gmail.com>
2021-05-25 12:00:04 -0500

    add logic to box-animations.component

--- src/app/main-login/main-login.component.ts
+++ src/app/main-login/main-login.component.ts
@@ -1,28 +1,13 @@
-import { animate, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-main-login',
   templateUrl: './main-login.component.html',
-  styleUrls: ['./main-login.component.scss'],
-  animations: [
-    trigger(
-      'enterAnimation', [
-        transition(':enter', [
-          style({transform: 'translateX(100%)', opacity: 0}),
-          animate('500ms', style({transform: 'translateX(0)', opacity: 1}))
-        ]),
-        transition(':leave', [
-          style({transform: 'translateX(0)', opacity: 1}),
-          animate('500ms', style({transform: 'translateX(100%)', opacity: 0}))
-        ])
-      ]
-    )
-  ]
+  styleUrls: ['./main-login.component.scss']
 })
 export class MainLoginComponent implements OnInit {
 
-  btnClicked =false;
+  
 
   constructor() { }
 
