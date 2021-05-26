01efbe2b41a2bd810892a497dfd461cdb4ffbc80
milton.velilla <milton.velillac@gmail.com>
2021-05-26 16:54:23 -0500

    add firebase config to environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDWZfpt9NQOd1Fx0g9KEOcuNjxKU_ENNb0",
+    authDomain: "angular-firebase-login-32c39.firebaseapp.com",
+    projectId: "angular-firebase-login-32c39",
+    storageBucket: "angular-firebase-login-32c39.appspot.com",
+    messagingSenderId: "1099271475513",
+    appId: "1:1099271475513:web:f78f0a11bd8ae118dfd8e8",
+    measurementId: "G-61QQS3MWBH"
+  }
 };
